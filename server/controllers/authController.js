1b68a88add70332f91d8da12dd20dbc5c5241e48
mahabubDevs <mdmahabubrahman800@gmail.com>
2025-01-23 21:39:54 +0600

    login and register backend complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,7 @@ const {check, validationResult} = require('express-validator');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { hashSync } = require('bcrypt');
 
 
 const authController = {
@@ -18,7 +19,7 @@ const authController = {
         check('password', 'Password is required').isLength({ min: 6 })
     ],
 
-    
+
     register: async (req, res) => {
         console.log(req.body);
         const { username, email, role, password } = req.body;
@@ -27,7 +28,7 @@ const authController = {
             return res.status(400).json({ msg: 'All fields are required' });
         }
         //check user already exists
-        const existingUser = await User.fineOne({ email });
+        const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ msg: 'User already exists' });
         }
@@ -41,6 +42,7 @@ const authController = {
             role,
             password: hashedPassword
         });
+
         //save user
         await user.save();
 
@@ -48,8 +50,39 @@ const authController = {
             msg: 'User created successfully'
         });
     },
-    login: (req, res) => {
-        res.send('Login route');
+    login: async (req, res) => {
+        const { email, password } = req.body;
+        //check if all fields are empty
+        if (!email || !password) {
+            return res.status(400).json({ msg: 'All fields are required' });
+        }
+        //check user exists
+        try {
+                const existingUser = await User.findOne({ email });
+            if (!existingUser) {
+                return res.status(400).json({ msg: 'User does not exist' });
+            }   
+            //check password
+            const validPassword = await bcrypt.compare(password, existingUser.password);
+            if (!validPassword) {
+                return res.status(400).json({ msg: 'Invalid credentials' });
+            }
+            //create token
+            const token = jwt.sign({ id: existingUser._id, role: existingUser.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            return res.status(200).json({
+                token,
+                user: {
+                    id: existingUser._id,
+                    username: existingUser.username,
+                    email: existingUser.email,
+                    role: existingUser.role,
+                    created_at: existingUser.created_at
+                }
+            });
+        } catch (error) {
+            //handel error
+            return res.status(500).json({ msg: error.message });
+        }
     },
 }
 module.exports = authController;
